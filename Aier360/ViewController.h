f2012ff9147d9e8d72ebb1731d41650f42923657
Stephen <379128008@qq.com>
2014-11-05 08:59:05 +0800

    Initial Commit

---
+++ Aier360/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Aier360
+//
+//  Created by Stephen Zhuang on 14/11/5.
+//  Copyright (c) 2014年 Stephen Zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Aier360/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Aier360
+//
+//  Created by Stephen Zhuang on 14/11/5.
+//  Copyright (c) 2014年 Stephen Zhuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
