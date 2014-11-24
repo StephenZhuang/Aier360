131e39d00045039a0f10145c7376fa6cdf0f90e8
Stephen <379128008@qq.com>
2014-11-24 14:27:56 +0800

    添加icon和launchimage

---
+++ Aier360/Controllers/校园/我的ic卡+打卡记录/ZXMyCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZXMyCardViewController.h
+//  Aier360
+//
+//  Created by Stephen Zhuang on 14/11/24.
+//  Copyright (c) 2014年 Zhixing Internet of Things Technology Co., Ltd. All rights reserved.
+//
+
+#import "ZXRefreshTableViewController.h"
+
+@interface ZXMyCardViewController : ZXRefreshTableViewController
+
+@end
