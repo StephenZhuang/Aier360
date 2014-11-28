4b415c553687f396f5ee471821fe3b1cf3412d47
Stephen <379128008@qq.com>
2014-11-28 17:29:21 +0800

    add card menu

---
+++ Aier360/Controllers/校园/我的ic卡+打卡记录/ZXCardHistoryMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZXCardHistoryMenuViewController.h
+//  Aier360
+//
+//  Created by Stephen Zhuang on 14/11/28.
+//  Copyright (c) 2014年 Zhixing Internet of Things Technology Co., Ltd. All rights reserved.
+//
+
+#import "ZXRefreshTableViewController.h"
+
+@interface ZXCardHistoryMenuViewController : ZXRefreshTableViewController
+
+@end
---
+++ Aier360/Controllers/校园/我的ic卡+打卡记录/ZXCardHistoryMenuViewController.m
@@ -0,0 +1,37 @@
+//
+//  ZXCardHistoryMenuViewController.m
+//  Aier360
+//
+//  Created by Stephen Zhuang on 14/11/28.
+//  Copyright (c) 2014年 Zhixing Internet of Things Technology Co., Ltd. All rights reserved.
+//
+
+#import "ZXCardHistoryMenuViewController.h"
+
+@interface ZXCardHistoryMenuViewController ()
+
+@end
+
+@implementation ZXCardHistoryMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
