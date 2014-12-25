e483f6480331e5f881b63c009de9d4ad48d0d7b8
Stephen <379128008@qq.com>
2014-12-25 11:13:40 +0800

    联系人接口

---
+++ Aier360/Models/ZXFollow.m
@@ -0,0 +1,13 @@
+//
+//  ZXFollow.m
+//  Aier360
+//
+//  Created by Stephen Zhuang on 14/12/25.
+//  Copyright (c) 2014年 Zhixing Internet of Things Technology Co., Ltd. All rights reserved.
+//
+
+#import "ZXFollow.h"
+
+@implementation ZXFollow
+
+@end
