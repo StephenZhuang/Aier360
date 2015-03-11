7cb0a4a5ccb9878d1f6e4f617e68f8050ac5251f
Stephen <379128008@qq.com>
2015-03-11 13:29:47 +0800

    联系人页面

--- Aier360/Controllers/联系人/ZXContactsMenuViewController.m
+++ Aier360/Controllers/联系人/ZXContactsMenuViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ZXContactsMenuViewController.h"
+#import "ZXMenuCell.h"
+#import "ZXContactHeader.h"
 
 @interface ZXContactsMenuViewController ()
 
@@ -17,9 +19,61 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-//    if (CURRENT_IDENTITY == zx) {
-//        <#statements#>
-//    }
+    if (CURRENT_IDENTITY == ZXIdentityParent) {
+        menuArray = @[@[@"好友"],@[@"班级列表"]];
+    } else if (CURRENT_IDENTITY == ZXIdentityStaff) {
+        menuArray = @[@[@"好友"],@[@"组织架构"]];
+    } else {
+        menuArray = @[@[@"好友"],@[@"组织架构",@"班级列表"]];
+    }
+    
+    [self.tableView registerClass:[ZXContactHeader class] forHeaderFooterViewReuseIdentifier:@"contactHeader"];
+    [self.tableView setExtrueLineHidden];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return menuArray.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [menuArray[section] count];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return 0.01;
+    } else {
+        return 30;
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 55;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return [[UIView alloc] initWithFrame:CGRectZero];
+    } else {
+        ZXContactHeader *contactHeader = [self.tableView dequeueReusableHeaderFooterViewWithIdentifier:@"contactHeader"];
+        [contactHeader.titleLabel setText:@"校园通讯录"];
+        return contactHeader;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    ZXMenuCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    [cell.logoImage setImage:[UIImage imageNamed:menuArray[indexPath.section][indexPath.row]]];
+    [cell.titleLabel setText:menuArray[indexPath.section][indexPath.row]];
+    
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
