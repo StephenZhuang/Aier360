edbbe2beb395d27b1b243252cd1f84bc7c2eb864
Stephen <379128008@qq.com>
2015-04-06 11:42:03 +0800

    优化扫一扫的性能

--- Aier360/Controllers/联系人/ZXQRCodeViewController.m
+++ Aier360/Controllers/联系人/ZXQRCodeViewController.m
@@ -57,6 +57,8 @@
     [_line autoPinEdge:ALEdgeRight toEdge:ALEdgeRight ofView:imageView withOffset:-40];
     [_line autoSetDimension:ALDimensionHeight toSize:2];
     [_line autoPinEdge:ALEdgeTop toEdge:ALEdgeTop ofView:imageView withOffset:10];
+    
+    [self setupCamera];
 }
 
 -(void)animation1
@@ -87,50 +89,64 @@
 -(void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    [_preview removeFromSuperlayer];
-    [self setupCamera];
+    if (_session && ![_session isRunning]) {
+        [_session startRunning];
+    }
     timer = [NSTimer scheduledTimerWithTimeInterval:.02 target:self selector:@selector(animation1) userInfo:nil repeats:YES];
 }
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [timer invalidate];
+}
+
 - (void)setupCamera
 {
-    // Device
-    _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    // 耗时的操作
+        // Device
+        _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        
+        // Input
+        _input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
+        
+        // Output
+        _output = [[AVCaptureMetadataOutput alloc]init];
+        //    [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+        [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+        
+        // Session
+        _session = [[AVCaptureSession alloc]init];
+        [_session setSessionPreset:AVCaptureSessionPresetHigh];
+        if ([_session canAddInput:self.input])
+        {
+            [_session addInput:self.input];
+        }
+        
+        if ([_session canAddOutput:self.output])
+        {
+            [_session addOutput:self.output];
+        }
+        
+        // 条码类型 AVMetadataObjectTypeQRCode
+        _output.metadataObjectTypes =@[AVMetadataObjectTypeQRCode];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        // 更新界面
+        // Preview
+        _preview =[AVCaptureVideoPreviewLayer layerWithSession:self.session];
+        _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        //    _preview.frame =CGRectMake(20,110,280,280);
+        _preview.frame = self.view.bounds;
+        [self.view.layer insertSublayer:self.preview atIndex:0];
+        // Start
+        [_session startRunning];
+    });
+});
     
-    // Input
-    _input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
-    
-    // Output
-    _output = [[AVCaptureMetadataOutput alloc]init];
-    //    [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-    [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-    
-    // Session
-    _session = [[AVCaptureSession alloc]init];
-    [_session setSessionPreset:AVCaptureSessionPresetHigh];
-    if ([_session canAddInput:self.input])
-    {
-        [_session addInput:self.input];
-    }
-    
-    if ([_session canAddOutput:self.output])
-    {
-        [_session addOutput:self.output];
-    }
-    
-    // 条码类型 AVMetadataObjectTypeQRCode
-    _output.metadataObjectTypes =@[AVMetadataObjectTypeQRCode];
-    
-    // Preview
-    _preview =[AVCaptureVideoPreviewLayer layerWithSession:self.session];
-    _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
-//    _preview.frame =CGRectMake(20,110,280,280);
-    _preview.frame = self.view.bounds;
-    [self.view.layer insertSublayer:self.preview atIndex:0];
     
     
     
-    // Start
-    [_session startRunning];
 }
 #pragma mark AVCaptureMetadataOutputObjectsDelegate
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
@@ -166,6 +182,7 @@
             }
         }
     } else {
+        [self.navigationController popViewControllerAnimated:YES];
         [[UIApplication sharedApplication] openURL:[NSURL URLWithString:stringValue]];
     }
 }
