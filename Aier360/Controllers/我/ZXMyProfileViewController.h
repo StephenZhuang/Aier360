d4293ccb68ab920790ec86c4a32ea1ced34529c2
Stephen <379128008@qq.com>
2015-04-14 16:06:25 +0800

    渐变的navigation

---
+++ Aier360/Controllers/我/ZXMyProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZXMyProfileViewController.h
+//  Aierbon
+//
+//  Created by Stephen Zhuang on 15/4/14.
+//  Copyright (c) 2015年 Zhixing Internet of Things Technology Co., Ltd. All rights reserved.
+//
+
+#import "ZXBaseViewController.h"
+
+@interface ZXMyProfileViewController : UITableViewController
+@property (nonatomic , weak) IBOutlet UIImageView *profileImage;
+@end
