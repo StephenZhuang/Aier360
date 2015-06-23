b653387523bb43056d8dfa89d5d8440c66cb9d82
Stephen <379128008@qq.com>
2015-06-23 20:25:52 +0800

    宝宝生日最多显示3个
    build++

--- Aier360/Controllers/联系人/ZXContactsViewController.m
+++ Aier360/Controllers/联系人/ZXContactsViewController.m
@@ -212,13 +212,13 @@
             [cell.titleLabel setText:[friend displayName]];
             NSArray *birthArray = [friend.babyBirthdays componentsSeparatedByString:@","];
             NSMutableArray *arr = [[NSMutableArray alloc] init];
-            for (NSString *birth in birthArray) {
-                NSString *babyStr = [ZXTimeHelper yearAndMonthSinceNow:birth];
-                [arr addObject:babyStr];
+            for (int i = 0; i < MIN(birthArray.count, 3); i++) {
+                NSString *birth = [ZXTimeHelper yearAndMonthSinceNow:[birthArray objectAtIndex:i]];
+                [arr addObject:birth];
             }
             NSString *str = [arr componentsJoinedByString:@"&"];
             if (str.length > 0) {
-                str = [NSString stringWithFormat:@"宝宝%@",str];
+                str = [NSString stringWithFormat:@"宝宝 %@",str];
             }
             [cell.addressLabel setText:str];
             
